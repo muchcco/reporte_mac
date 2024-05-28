c7fb371a08647af9b25d1fc4f0c088db5d4d1b9c
kevinmuchcco <kevinmuchcco@gmail.com>
2024-05-28 11:24:04 -0500

    dash

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,36 +1,35 @@
 @extends('layouts.layout')
 
-@section('title_page')
-<div class="col-sm-12">
-  <div class="page-title-box">
-      <div class="row">
-          <div class="col">
-              <h4 class="page-title">Starer</h4>
-              <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="javascript:void(0);">Dastone</a></li>
-                  <li class="breadcrumb-item"><a href="javascript:void(0);">Pages</a></li>
-                  <li class="breadcrumb-item active">Starer</li>
-              </ol>
-          </div><!--end col-->
-          <div class="col-auto align-self-center">
-              <a href="#" class="btn btn-sm btn-outline-primary" id="Dash_Date">
-                  <span class="day-name" id="Day_Name">Today:</span>&nbsp;
-                  <span class="" id="Select_date">Jan 11</span>
-                  <i data-feather="calendar" class="align-self-center icon-xs ms-1"></i>
-              </a>
-              <a href="#" class="btn btn-sm btn-outline-primary">
-                  <i data-feather="download" class="align-self-center icon-xs"></i>
-              </a>
-          </div><!--end col-->  
-      </div><!--end row-->                                                              
-  </div><!--end page-title-box-->
-</div><!--end col-->
-    
-@endsection
-
 
 
 @section('main')
  <!-- Default box -->
- 
+ <div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="row align-items-center">
+                    <div class="col">                      
+                        <h4 class="card-title">Reporte obtenido por todos los canales</h4>  
+                        {{-- <p>Entidades presentes el día de hoy ({{ Carbon\Carbon::now()->format('d-m-Y') }})</p>                    --}}
+                    </div><!--end col-->
+                    {{-- <div class="col-auto"> 
+                        <div class="dropdown">
+                            <a href="#" class="btn btn-sm btn-outline-light dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                               Todos<i class="las la-angle-down ms-1"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a class="dropdown-item" href="#">Asistio</a>
+                                <a class="dropdown-item" href="#">No Asistio</a>
+                            </div>
+                        </div>         
+                    </div><!--end col--> --}}
+                </div>  <!--end row-->                                  
+            </div><!--end card-header-->
+            <div class="card-body">
+                {{-- <iframe src="" frameborder="0" class="power-bi-dash"></iframe> --}}
+                <iframe title="mac_dat_tot" width="1500" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiYzJhOTkwYTMtNmNkZS00ZGUwLTlmY2EtOTg3MTkzYWUxYzVhIiwidCI6IjM0YjQ4ZTRlLTI1MTktNDA2MC1hMDllLTViMDVkOTAxYTRkNyJ9" frameborder="0" allowFullScreen="true"></iframe>
+            </div>
+        </div>
+    </div>
 @endsection
